f9b4f6654bf2772ff294aa0806d533e0ce733738
ArmanSakurov <a.sakurov@skyeng.ru>
2023-01-01 15:31:50 +0500

    [android] added changes to baseUrl

--- src/test/java/config/BrowserstackConfig.java
+++ src/test/java/config/BrowserstackConfig.java
@@ -15,30 +15,30 @@ public interface BrowserstackConfig extends Config {
     @DefaultValue("89Baz9pmNscurTVCUVWk")
     String key();
 
-    @Key("app")
-    @DefaultValue("bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c")
-    String app();
-
-    @Key("device")
-    @DefaultValue("Google Pixel 3")
-    String device();
-
-    @Key ("osVersion")
-    @DefaultValue("9.0")
-    String osVersion();
-
-    @Key("project")
-    @DefaultValue("First Java Project")
-    String project();
-
-    @Key("build")
-    @DefaultValue("browserstack-build-2")
-    String build();
-
-    @Key("name")
-    @DefaultValue("first_test")
-    String name();
-
-    @Key ("baseUrl")
-    String baseUrl();
+//    @Key("app")
+//    @DefaultValue("bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c")
+//    String app();
+//
+//    @Key("device")
+//    @DefaultValue("Google Pixel 3")
+//    String device();
+//
+//    @Key ("osVersion")
+//    @DefaultValue("9.0")
+//    String osVersion();
+//
+//    @Key("project")
+//    @DefaultValue("First Java Project")
+//    String project();
+//
+//    @Key("build")
+//    @DefaultValue("browserstack-build-2")
+//    String build();
+//
+//    @Key("name")
+//    @DefaultValue("first_test")
+//    String name();
+//
+//    @Key ("baseUrl")
+//    String baseUrl();
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -21,7 +21,6 @@ public class TestBase {
     @BeforeEach
     void startDriver() {
         addListener("AllureSelenide", new AllureSelenide());
-
         open();
     }
 
