1538ec985d47c960caa3355bc7898e4c5b3f80ec
ArmanSakurov <a.sakurov@skyeng.ru>
2023-01-01 15:54:58 +0500

    [android] changed locator

--- src/test/java/tests/android/browserstack_sample/WikiSearchTests.java
+++ src/test/java/tests/android/browserstack_sample/WikiSearchTests.java
@@ -1,23 +1,24 @@
 package tests.android.browserstack_sample;
 
+import com.codeborne.selenide.CollectionCondition;
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.Test;
 import tests.TestBase;
 
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
-public class SelenideSearchTests extends TestBase {
+public class WikiSearchTests extends TestBase {
 
     @Test
     void successSearchTest() {
         $(AppiumBy.accessibilityId("Search Wikipedia")).click();
-        $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Java");
-        $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(sizeGreaterThan(0));
+        $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Tesla");
+        $$(AppiumBy.className("android.widget.TextView"))
+                .shouldHave(CollectionCondition.sizeGreaterThan(0));
 
         $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).click();
         $(AppiumBy.className("android.view.View")).click();
-        $(AppiumBy.className("android.widget.TextView")).shouldHave(text("Java"));
+        $(AppiumBy.className("android.widget.TextView")).shouldHave(text("Tesla"));
     }
 }
