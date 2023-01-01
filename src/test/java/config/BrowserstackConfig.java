54dad599f7f1b749920132f876dceb197c698c47
ArmanSakurov <a.sakurov@skyeng.ru>
2023-01-01 15:16:36 +0500

    [android] added changes to BrowserstackConfig.java

--- src/test/java/config/BrowserstackConfig.java
+++ src/test/java/config/BrowserstackConfig.java
@@ -8,27 +8,35 @@ import org.aeonbits.owner.Config;
 public interface BrowserstackConfig extends Config {
 
     @Key("user")
+    @DefaultValue("bsuser_0bae97")
     String user();
 
     @Key("key")
+    @DefaultValue("89Baz9pmNscurTVCUVWk")
     String key();
 
     @Key("app")
+    @DefaultValue("bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c")
     String app();
 
     @Key("device")
+    @DefaultValue("Google Pixel 3")
     String device();
 
     @Key ("osVersion")
+    @DefaultValue("9.0")
     String osVersion();
 
     @Key("project")
+    @DefaultValue("First Java Project")
     String project();
 
     @Key("build")
+    @DefaultValue("browserstack-build-2")
     String build();
 
     @Key("name")
+    @DefaultValue("first_test")
     String name();
 
     @Key ("baseUrl")
